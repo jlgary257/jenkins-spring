f3c47684535dc2e7317f89beacde4947e5436703
JL Gary <jlgary257@gmail.com>
2026-03-11 11:16:41 +0800

    Initial commit

---
+++ src/main/java/com/example/Jenkinspring/JenkinspringApplication.java
@@ -0,0 +1,13 @@
+package com.example.Jenkinspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Jenkinspring/JenkinspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Jenkinspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
