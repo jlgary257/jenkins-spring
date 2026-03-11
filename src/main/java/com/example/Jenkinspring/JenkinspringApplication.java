494bf262d926f252d0951007cd034d97a3bbd58b
JL Gary <jlgary257@gmail.com>
2026-03-11 11:40:12 +0800

    New

--- src/main/java/com/example/Jenkinspring/JenkinspringApplication.java
+++ src/main/java/com/example/Jenkinspring/JenkinspringApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JenkinspringApplication {
 
+@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(JenkinspringApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinspringApplication.class, args);
 	}
